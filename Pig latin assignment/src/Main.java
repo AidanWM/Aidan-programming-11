a265c42a913b062db76cc916d749c199061ec989
AidanWM <43757549+AidanWM@users.noreply.github.com>
2018-10-30 13:44:13 -0700

    Encryption assignment

---
+++ Pig latin assignment/src/Main.java
@@ -0,0 +1,173 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //What word to pig latinafie
+        String name = "Potato";
+
+        System.out.print(name.substring(name.length() - 1));
+
+        for(int i = 0;i < name.length()- 1; i++){
+
+            if(name.substring(i,i + 1).equals("a")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("b")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("c")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("d")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("e")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("f")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("g")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("h")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("i")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("j")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("k")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("l")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("m")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("n")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("o")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("p")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("q")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("r")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("s")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("t")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("u")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("v")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("w")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("x")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("y")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("z")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("A")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("B")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("C")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("D")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("E")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("F")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("G")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("H")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("I")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("J")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("K")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("L")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("M")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("N")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("O")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("P")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("Q")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("R")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("S")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("T")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("U")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("V")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("W")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("X")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("Y")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals("Z")){
+                System.out.print(name.substring(i, i + 1));
+            }
+            if(name.substring(i,i + 1).equals(" ")){
+                System.out.print(name.substring(i, i + 1));
+            }
+        }
+        System.out.print("ay");
+    }
+}
